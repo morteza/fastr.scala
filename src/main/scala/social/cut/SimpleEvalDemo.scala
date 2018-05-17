50989486186f34d626231d9ac671ff318e202743
Morteza Ansarinia <ansarinia@me.com>
2018-05-17 19:37:12 +0430

    Rewrite simple eval using context builder

--- src/main/scala/social/cut/SimpleEvalDemo.scala
+++ src/main/scala/social/cut/SimpleEvalDemo.scala
@@ -3,7 +3,7 @@ package social.cut
 import org.graalvm.polyglot._
 
 object SimpleEvalDemo extends App {
-  val context = Context.create("R")
-  val result: Value = context.eval("R", "2^3").execute()
-  println(s"Result from FastR is ${result.asString}")
+  val context = Context.newBuilder("R").allowAllAccess(true).build()
+  val result: Value = context.eval("R", "2^3")
+  println(s"Result from FastR is ${result.asDouble}")
 }
